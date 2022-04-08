5df1e83a22721f29f0eef8ac4dda6ac3392e5d31
Anzhelika <anzh201419@yandex.ua>
2022-04-08 17:25:11 +0300

    Урок 5

--- fp/src/App.vue
+++ fp/src/App.vue
@@ -1,106 +1,57 @@
 <template>
   <div id="app">
-    <div class="wrapper">
-      <header class="title">My personal costs</header>
-      <div>My total {{ getFPV }}</div>
-      <main>
-        <AddPaumentForm @addNewPayment="addDate" />
-        <PaymentsDisplay :list="currentElements" />
-        <MyPagination
-          :length="paymentsList.length"
-          :n="n"
-          :cur="cur"
-          @changePage="onChangePage"
-        />
-      </main>
-    </div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link> |
+      <router-link to="/about">About</router-link>
+      <div @click="goToPageAbout">Go About</div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-import PaymentsDisplay from "./components/PaymentsDisplay.vue";
-import AddPaumentForm from "./components/AddPaymentForm.vue";
-import MyPagination from "./components/MyPagination.vue";
-import { mapMutations } from "vuex";
 export default {
   name: "App",
-  components: {
-    PaymentsDisplay,
-    AddPaumentForm,
-    MyPagination,
-  },
-  data() {
-    return {
-      n: 5,
-      cur: 1,
-    };
-  },
-  computed: {
-    /*   ...mapGetters(["getFullPaymentValue", "getPaymentList"]), */
-    getFPV() {
-      return this.$store.getters.getFullPaymentValue;
-    },
-    paymentsList() {
-      return this.$store.getters.getPaymentList;
-    },
-    currentElements() {
-      return this.paymentsList.slice(
-        this.n * (this.cur - 1),
-        this.n * (this.cur - 1) + this.n
-      );
-    },
-  },
   methods: {
-    ...mapMutations(["setPaymentListData"]),
-    fetchData() {
-      return [
-        {
-          date: "28.03.2020",
-          category: "Food",
-          value: 169,
-          id: "1",
+    goToPageAbout() {
+      this.$router.push({
+        name: "about",
+        params: {
+          isTrial: 0,
         },
-        {
-          date: "24.03.2020",
-          category: "Transport",
-          value: 360,
-          id: "2",
+        query: {
+          isTrial: true,
         },
-        {
-          date: "24.03.2020",
-          category: "Food",
-          value: 532,
-          id: "3",
-        },
-      ];
-    },
-    addDate(data) {
-      this.$store.commit("addDataPaymentsList", data);
-    },
-    onChangePage(p) {
-      this.cur = p;
+      });
     },
   },
   created() {
-    this.setPaymentListData(this.fetchData());
-  },
-  mounted() {
-    this.$store.dispatch("fetchData");
+    console.log(this.$router);
   },
 };
 </script>
 
-<style lang='scss' scoped>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-.title {
-  font-size: 30px;
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
